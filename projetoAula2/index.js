6555539469a3d9b3ad9d142569480f9b7040f57a
Robson Szabo <robson.szabo@si.unifacisa.edu.br>
2019-02-21 22:03:53 -0300

    Example of compact and descompact archive

---
+++ projetoAula2/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const archiver = require('archiver');
+const extract = require('extract-zip');
+const utils = require('./utils');
+
+utils.inputVerifications();
+
+if (utils.isCompactProcess() ) {
+    const Robson = process.argv[3];
+    const zipFileNameToCreate = process.argv[4];
+    
+    let output = fs.createWriteStream(zipFileNameToCreate);
+    let compactor = archiver(zipFileNameToCreate.split('.')[1]);
+    
+    output.on('close', () => {
+        console.log(compactor.pointer() + ' bytes totais!')
+    })
+    
+    compactor.pipe(output);
+    compactor.glob(Robson + '/*');
+    compactor.finalize();
+}
+
+if (utils.isUncompactProcess()) {
+    const zipFileNameToUncompact = process.argv[3];
+    const destinyOfFile = process.argv[4];
+
+    extract(zipFileNameToUncompact, {dir: __dirname + '/' + destinyOfFile}, function (err) {
+        console.error(err);
+    })
+}
